08b2d164ac89eeb3e85882d70541cc9c0dc56328
yangcaizhi <731074376@qq.com>
2021-07-05 11:26:39 +0800

    yang

--- js/config.js
+++ js/config.js
@@ -2,7 +2,9 @@ window.g = {
   baseURL: "http://admin.west.bilan.wyz.ltd/mock-server", //api接口地址
   wxOauth: true, //启用微信授权
   appId: "wx6a764f7fa9554e44", //公众号Id
+  allUrl: "http://west.bilan.wyz.ltd",
   redirectUrl: "http://west.bilan.wyz.ltd/WxAuth", //oatuh授权回调地址
   // callbackUrl: "http://fuwu.wyz.ltd/oauth_callback", //oatuh授权回调获取信息访问地址
   callbackUrl: "/front/wechat/oauth",
+  
 };
